c5cbcdf6f8810a19ca879cc7ba75e650c8439266
brando vidal <brandojesus011@gmail.com>
2019-09-10 19:07:11 -0500

    upload excel file

--- src/app/services/exporter.service.ts
+++ src/app/services/exporter.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
-const EXCEL_TYPE = 
+const EXCEL_TYPE =
 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; charset=UTF-8';
 const EXCEL_EXT = '.xlsx';
 
@@ -12,9 +12,9 @@ export class ExporterService {
 
   exportToExcel(json: any[], excelFileName: string): void {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-    const workbook: XLSX.WorkBook = { 
-      Sheets: {'data': worksheet} , 
-      SheetNames: ['data'] 
+    const workbook: XLSX.WorkBook = {
+      Sheets: { data: worksheet} ,
+      SheetNames: ['data']
     };
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     // call method buffer and filename
